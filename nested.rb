ff813e5ebed3928679535c92665f73a0d187c492
Alouiscious Sims <info@reneesoul.com>
2019-07-15 07:17:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 return programmer_hash[:dennis_ritchie][:languages]
 return programmer_hash
 end
