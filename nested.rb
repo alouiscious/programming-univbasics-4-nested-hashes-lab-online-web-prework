6bc63eb0355c2fa9db6c1e206e9392ca3080d45c
Alouiscious Sims <info@reneesoul.com>
2019-07-15 07:02:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,10 +93,7 @@ programmer_hash[:yukihiro_matsumoto] = {}
 # programmer_hash[:yukihiro_matsumoto] = "known_for", "languages"
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
-	# [:known_for] = "Ruby"
-	# = ("Ruby", "languages")
-	#  = ["LISP", "C"]
-
+	
 return programmer_hash
 return programmer_hash[:yukihiro_matsumoto]
 return programmer_hash[:yukihiro_matsumoto][:known_for]
